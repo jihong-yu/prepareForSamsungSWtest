840546044de1365c7d80f195bd78d81b814a7e4d
HomePC <jimdac@naver.com>
2022-01-26 23:29:15 +0900

    2022-01-26

--- 정렬/백준2628(종이 자르기).py
+++ 정렬/백준2628(종이 자르기).py
@@ -1,14 +1,14 @@
-width, height = map(int, input().split())
-N = int(input())
-info_width = [0, width]  # 가로 길이를 구하기위해 점들을 저장할 리스트
-info_height = [0, height]  # 세로 길이를 구하기 위해 점들을 저장할 리스트
+width, height = map(int, input().split()) #가로 세로 길이 입력
+N = int(input()) #점 개수 입력
+info_width = [0, width]  # 가로 길이를 구하기위해 세로점들을 저장할 리스트
+info_height = [0, height]  # 세로 길이를 구하기 위해 가로점들을 저장할 리스트
 for i in range(N):  # 입력 받은 개수 만큼 반복문 돈다.
     temp = list(map(int, input().split()))
 
     if temp[0] == 0:  # 만약 가로점을 입력받았다면
         info_height.append(temp[1])  # 세로 길이 리스트에 값들을 저장해준다.
     else:  # 만약 세로 점을 입력받았다면
-        info_width.append(temp[1])  # 가로 길이 리스트에 세로길이들을 저장해준다.
+        info_width.append(temp[1])  # 가로 길이 리스트에 값들을 저장해준다.
 
 info_width.sort()  # 오름차순 정렬
 info_height.sort()  # 오름차순 정렬
